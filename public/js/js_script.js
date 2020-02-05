d202bc3958776c9da3b671c2c087364150883d9b
Albin <albin.lundholm@gmail.com>
2020-02-05 13:30:51 +0100

    Task 4-3

---
+++ public/js/js_script.js
@@ -0,0 +1,28 @@
+function menuItem(name, kcal, gluten, lactose, ingredient, imgPath) {
+    this.name = name;
+    this.kcal = kcal;
+    this.gluten = gluten;
+    this.lactose = lactose;
+    this.ingredient = ingredient;
+    this.imgPath = imgPath;
+}
+
+function printItem(item) {
+    return [item.name, item.kcal];
+}
+
+let item1 = new menuItem("first burger", 500000, true, false, "Cheese", "https://cdn-rdb.arla.com/Files/arla-se/1361781217/43b7c050-b61b-4baa-8eb6-5a8ebb896bc4.jpg?crop=(0,220,0,-250)&w=1269&h=545&ak=f525e733&hm=7f223915");
+
+let item2 = new menuItem("second burger", 5, false, true, "No cheese", "https://www.studentkortet.se/wp-content/uploads/2019/12/burger-king-website.jpg");
+
+let item3 = new menuItem("third burger", 500, false, false, "Pickeled onions", "https://www.56kilo.se/wp-content/uploads/2019/09/03092019-11.jpg");
+
+let item4 = new menuItem("fourth burger", 5000, true, true, "Everything", "https://media-cdn.tripadvisor.com/media/photo-s/18/9a/d7/1c/the-devils-steak-burger.jpg");
+
+let item5 = new menuItem("fiifth burger", 50, false, false, "Nothing", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Bulka_mala_pszenna.jpg/380px-Bulka_mala_pszenna.jpg");
+
+console.log(printItem(item1));
+console.log(printItem(item2));
+console.log(printItem(item3));
+console.log(printItem(item4));
+console.log(printItem(item5));
