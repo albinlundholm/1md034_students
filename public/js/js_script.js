d362367265ed8a7dc94a7e5e717758b4d6aadf35
Albin <albin.lundholm@gmail.com>
2020-02-05 18:40:19 +0100

    Task 4-8

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -6,7 +6,7 @@ function menuItem(name, kcal, gluten, lactose, ingredient, imgPath) {
     this.ingredient = ingredient;
     this.imgPath = imgPath;
 }
-
+/*
 function printItem(item) {
 	let itemString = item.name + ' Calories: ' + item.kcal;
   if (item.gluten) {
@@ -17,9 +17,9 @@ function printItem(item) {
   }
     return itemString;
 }
-
+*/
 let burgerList = [new menuItem("first burger", 500000, true, false, "Cheese", "https://cdn-rdb.arla.com/Files/arla-se/1361781217/43b7c050-b61b-4baa-8eb6-5a8ebb896bc4.jpg?crop=(0,220,0,-250)&w=1269&h=545&ak=f525e733&hm=7f223915"), new menuItem("second burger", 5, false, true, "No cheese", "https://www.studentkortet.se/wp-content/uploads/2019/12/burger-king-website.jpg"), new menuItem("third burger", 500, false, false, "Pickeled onions", "https://www.56kilo.se/wp-content/uploads/2019/09/03092019-11.jpg"), new menuItem("fourth burger", 5000, true, true, "Everything", "https://media-cdn.tripadvisor.com/media/photo-s/18/9a/d7/1c/the-devils-steak-burger.jpg"), new menuItem("fifth burger", 50, false, false, "Nothing", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Bulka_mala_pszenna.jpg/380px-Bulka_mala_pszenna.jpg")];
-
+/*
 let menu = document.getElementById("menu");
 
 for (var burger of burgerList) {
@@ -60,7 +60,7 @@ for (var burger of burgerList) {
     box.appendChild(infoList);
     menu.appendChild(box);
 }
-
+*/
 /*
 let burgerList = document.getElementById("burgerList");
 for (var burger of menu) 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,20 +1,6 @@
-/*
-<div id="myID">
-    <h1>Välj en burgare</h1>
-    <ul id="burgerList">
-    <li v-for="burger in burgerMenu">
-      {{ burger.name }} Calories: {{burger.kcal}}
-      <span v-if="burger.gluten == true" >Contains gluten</span>
-      <span v-if="burger.lactose == true" >Contains lactose</span>
-    </li>
-    </ul>
-</div>
-*/
-/*
 const vm = new Vue({
-    el: '#burgerList',
+    el: '#menu',
     data: {
-  	burgerMenu: menu
+  	burgerMenu: burgerList
     }
 })
-*/
