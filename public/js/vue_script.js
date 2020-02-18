12b4bbf36da337e06ac7759b0f371e4a10b63981
Albin <albin.lundholm@gmail.com>
2020-02-18 17:23:59 +0100

    Task 5-3

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -4,7 +4,9 @@ const socket = io();
 const vm = new Vue({
     el: '#main',
     data: {
+	orderCount: 0,
 	orders: {},
+	order: {details: {x: 0, y: 0}},
   	burgerMenu: burgerList,
 	infoSent: false, 
 	name: "", 
@@ -13,24 +15,9 @@ const vm = new Vue({
 	//house: "",
 	payment: 'Credit/debit card',
 	gender: 'male', 
-	pickedBurgers: []
+	pickedBurgers: [], 
+	
     } ,
-    created: function() {
-	/* When the page is loaded, get the current orders stored on the server.
-	 * (the server's code is in app.js) */
-	socket.on('initialize', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-
-	/* Whenever an addOrder is emitted by a client (every open map.html is
-	 * a client), the server responds with a currentQueue message (this is
-	 * defined in app.js). The message's data payload is the entire updated
-	 * order object. Here we define what the client should do with it.
-	 * Spoiler: We replace the current local order object with the new one. */
-	socket.on('currentQueue', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-    },
     methods: {
 	sendInfo: function() {
 	    if (!this.infoSent) {
@@ -40,13 +27,8 @@ const vm = new Vue({
 	    }
 	},
 	getNext: function() {
-	    /* This function returns the next available key (order number) in
-	     * the orders object, it works under the assumptions that all keys
-	     * are integers. */
-	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-		return Math.max(last, next);
-	    }, 0);
-	    return lastOrder + 1;
+	    this.orderCount++;
+	    return this.orderCount;
 	},
 	addOrder: function(event) {
 	    /* When you click in the map, a click event object is sent as parameter
@@ -54,18 +36,29 @@ const vm = new Vue({
 	     * The click event object contains among other things different
 	     * coordinates that we need when calculating where in the map the click
 	     * actually happened. */
+	    /*let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };*/
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    /*x: this.order.x - 10 - offset.x,
+		    y: this.order.y - 10 - offset.y,*/
+		    x: this.order.details.x,
+		    y: this.order.details.y,
+		},
+		orderItems: this.pickedBurgers,
+	    });
+	},
+	displayOrder: function(event) {
 	    let offset = {
 		x: event.currentTarget.getBoundingClientRect().left,
 		y: event.currentTarget.getBoundingClientRect().top,
 	    };
-	    socket.emit('addOrder', {
-		orderId: this.getNext(),
-		details: {
-		    x: event.clientX - 10 - offset.x,
-		    y: event.clientY - 10 - offset.y,
-		},
-		orderItems: ['Beans', 'Curry'],
-	    });
+	    
+	    this.order.details.x = event.clientX - 10 - offset.x;
+	    this.order.details.y = event.clientY - 10 - offset.y;
 	}
     }
 })
